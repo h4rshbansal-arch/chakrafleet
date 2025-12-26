5b71b248cfde2842b05322f1d3a2e176ba92e9ab
Harsh Bansal <bansal.boy.only@gmail.com>
2025-12-26 16:33:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-914529814-530d4",
+  "appId": "1:514031696298:web:5e9148432254c4f01e53d3",
+  "apiKey": "AIzaSyBPVdDTo3qSixyxGcQt8OH5pdy-8yYuHfA",
+  "authDomain": "studio-914529814-530d4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "514031696298"
+};
