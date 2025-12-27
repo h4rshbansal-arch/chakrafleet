0114fb5ec6d97f4b486f78fecb1c19eb1e7c6066
Harsh Bansal <bansal.boy.only@gmail.com>
2025-12-27 10:44:24 +0000

    footer at every page bold and visible

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <footer className="mt-auto p-4 text-center text-sm text-muted-foreground sm:px-6">
+    <footer className="mt-auto p-4 text-center text-base font-bold text-foreground sm:px-6">
       <p>- Software By Harsh Bansal</p>
     </footer>
   );
