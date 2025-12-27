32c737620ff1fffd5873e474f02daa870e9bb189
Harsh Bansal <bansal.boy.only@gmail.com>
2025-12-27 10:40:57 +0000

    Make the footer : - Software By Harsh Bansal

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export function Footer() {
   return (
     <footer className="mt-auto p-4 text-center text-sm text-muted-foreground sm:px-6">
-      <p>Made by Harsh Bansal</p>
+      <p>- Software By Harsh Bansal</p>
     </footer>
   );
 }
